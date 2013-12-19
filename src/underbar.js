43998829f0fad63c4549d9e0919765b61345720b
Warren Ng <warrenng23@gmail.com>
2013-12-19 17:34:36 -0500

    Implemented invoke, when provided a function reference

--- src/underbar.js
+++ src/underbar.js
@@ -134,6 +134,9 @@ var _ = { };
   // Calls the method named by methodName on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item) {
+      return functionOrKey.apply(item);
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
