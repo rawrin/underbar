7797e39ae728cedb78ed663a36b53e2679b10ad6
Warren Ng <warrenng23@gmail.com>
2013-12-26 14:37:46 -0500

    Implemented extend

--- src/underbar.js
+++ src/underbar.js
@@ -234,6 +234,14 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(Array.prototype.slice.call(arguments, 1), function(source) {
+      if (source) {
+        for (var prop in source) {
+          obj[prop] = source[prop];
+        }
+      };
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
